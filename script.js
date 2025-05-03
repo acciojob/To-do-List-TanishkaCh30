4684fe8e3caa074205b23c1a0984279cf4ed935c
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-05-03 21:20:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-//your code here
+let button = document.getElementById("addTodoBtn");
+let ol =  document.getElementById("todoList");
+let input = document.getElementById("newTodoInput");
+
+
+button.addEventListener("click",()=>{
+
+	let li = document.createElement("li");
+	li.innerText = input.value;
+	ol.appendChild(li);
+	input.value="";
+})
+
 
